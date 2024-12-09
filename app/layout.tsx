d47b984c1faf61e4300af29c0d165f41290cb122
Kryslin Rass <144601674+kryrass@users.noreply.github.com>
2024-12-09 11:14:16 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,6 @@ import Link from "next/link"
 import "./globals.css"
 import { GeistSans } from "geist/font/sans"
 
-const defaultUrl = process.env.NEXT_PUBLIC_SITE_URL || `https://${process.env.VERCEL_URL}` || "http://localhost:3000";
-
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000"
